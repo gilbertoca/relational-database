6d0217750aa7110144bb7e6a6dea1f84f14b4da5
Gilberto Caetano de Andrade <3083703+gilbertoca@users.noreply.github.com>
2023-03-29 08:47:43 -0300

    Create create-table.sql

---
+++ create-table.sql
@@ -0,0 +1,17 @@
+--DROP TABLE games;
+--DROP TABLE teams;
+
+CREATE TABLE games (
+    game_id SERIAL PRIMARY KEY,
+    year integer NOT NULL,
+    round character varying(20) NOT NULL,
+    winner_id integer NOT NULL REFERENCES teams(team_id),
+    opponent_id integer NOT NULL REFERENCES teams(team_id),
+    winner_goals integer NOT NULL,
+    opponent_goals integer NOT NULL
+);
+
+CREATE TABLE teams (
+    team_id SERIAL PRIMARY KEY,
+    name character varying(20) NOT NULL UNIQUE
+);
